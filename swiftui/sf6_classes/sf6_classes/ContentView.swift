43e09565a6bfa70491b40f76fd786ee2659548cf
Nick Fox-Gieg <nick@fox-gieg.com>
2024-03-06 16:52:28 -0500

    Update ContentView.swift

--- swiftui/sf6_classes/sf6_classes/ContentView.swift
+++ swiftui/sf6_classes/sf6_classes/ContentView.swift
@@ -1,39 +1,53 @@
 import SwiftUI
 
-class Dot {
-    var pos = CGPoint(x: 0.0, y: 0.0)
-    var s = 50.0
-    var speed = CGFloat.random(in: 1..<5)
-    var target = CGPoint(x: CGFloat.random(in: 0..<640), y: CGFloat.random(in: 0..<360.0))
-
-    var fillOrig = Color(red: 0.0, green: 0.392, blue: 1.0)
-    var fillHit = Color(red: 0.0, green: 0.784, blue: 1.0)
-    var fillNow = Color(red: 0.0, green: 0.0, blue: 0.0)
+public class Dot {
+    var pos: CGPoint
+    var s: CGFloat
+    var speed: CGFloat
+    var target: CGPoint
+    var fillOrig:Color
+    var fillHit:Color
+    var fillNow:Color
+    var fps:CGFloat
     
-    init(){
-        print("Hello World")
-        fillNow = fillOrig
+    init(s: CGFloat){
+        self.target = CGPoint(x: CGFloat.random(in: 0..<640), y: CGFloat.random(in: 0..<360.0))
+        self.speed = CGFloat.random(in: 1..<5)
+        self.s = s
+        self.pos = CGPoint(x: 0.0, y: 0.0)
+        self.fillOrig = Color(red: 0.0, green: 0.392, blue: 1.0)
+        self.fillHit = Color(red: 0.0, green: 0.784, blue: 1.0)
+        self.fillNow = self.fillOrig
+        self.fps = 1.0 / 60.0
     }
     
-    func update() {
-        
-    }
-    
-    func draw() {
-        
+    func test() {
+        //
     }
     
     func run() {
-        
+        Circle()
+            .strokeBorder(.black, lineWidth: 2)
+            .background(Circle().fill(.white))
+            .frame(width: self.s, height: self.s)
+            .position(x: self.pos.x, y: self.pos.y)
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: self.fps, repeats: true) { timer in
+                    withAnimation(Animation.linear(duration: 0.0)) {
+                        self.pos.x += self.speed
+                        
+                        if self.pos.x > 640 || self.pos.x < 0 {
+                            self.speed *= -1.0
+                        }
+                    }
+                }
+            }
     }
 }
 
 struct ContentView: View {
     let numDots = 10
-    //var dots = []
-    //ForEach(0..<numDots, id: \.self) {
-        //dots.append(Dot())
-    //}
+    @State var dots: [Dot] = []
     
     @State var x = [ 100.0, 100.0, 100.0, 100.0, 100.0 ]
     @State var speed = [ 1.0, 2.0, 3.0, 4.0, 5.0 ]
@@ -41,10 +55,15 @@ struct ContentView: View {
     let s = 50.0
     let fps = 1.0 / 60.0
     @State var width = 0.0
-    
+   
     var body: some View {
+        let dot = Dot(s: 50.0)
+        dots.append(dot)
+
         ZStack {
             Color.gray
+            
+            dots[0].run()
             
             ForEach(0..<x.count, id: \.self) { i in
                 Circle()
